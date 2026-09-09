9e6fe873666d5b9227a541920b96e99eb7b0d584
emanicewalker27-prog <emanicewalker27@gmail.com>
2026-09-09 10:06:54 -0500

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/active/go/',
-    bare:'https://bare.benrovers.managed.ovh/',
+    bare: 'https://tomphttp.mrvrc.workers.dev/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/active/uv/uv.handler.js',
